742da3ad041bdbc3fc6e40ae34339ab4475b9905
ZeckCode <163707982+ZeckCode@users.noreply.github.com>
2026-03-29 10:14:45 +0800

    s

--- BackEnd/reminders/views.py
+++ BackEnd/reminders/views.py
@@ -1,3 +1,17 @@
+
+#Reminders views.py
+from rest_framework import generics, status
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from django.contrib.auth import get_user_model
+
+
+from accounts.models import UserProfile
+from .models import Reminder
+from .serializers import ReminderSerializer
+from finance.models import Transaction
+
 @api_view(["POST"])
 @permission_classes([IsAuthenticated])
 def send_paid_notification(request, transaction_id):
@@ -52,16 +66,8 @@ def send_paid_notification(request, transaction_id):
         },
         status=status.HTTP_201_CREATED,
     )
-#Reminders views.py
-from rest_framework import generics, status
-from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
-from django.contrib.auth import get_user_model
 
-from .models import Reminder
-from .serializers import ReminderSerializer
-from finance.models import Transaction
+
 
 User = get_user_model()
 
@@ -283,7 +289,7 @@ def send_performance_reminder(request):
     if not student_id:
         return Response({"detail": "student_id is required."}, status=400)
 
-    from accounts.models import UserProfile
+    
 
     try:
         profile = UserProfile.objects.select_related("user").get(user_id=student_id)
