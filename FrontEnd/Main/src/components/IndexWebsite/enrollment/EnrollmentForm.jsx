bb4cea1b12239594e6e310cc3ab815dcc900a217
ZeckCode <163707982+ZeckCode@users.noreply.github.com>
2026-03-31 13:29:33 +0800

    Update EnrollmentForm.jsx

--- FrontEnd/Main/src/components/IndexWebsite/enrollment/EnrollmentForm.jsx
+++ FrontEnd/Main/src/components/IndexWebsite/enrollment/EnrollmentForm.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useMemo, useRef, useState } from "react";
-// import { useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { apiFetch } from "../../api/apiFetch";
 import { STEP_KEYS } from "./constants";
 import {
